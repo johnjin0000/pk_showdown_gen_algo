274902b04d1c39effc0adc4eb153998384a1e3c0
SpencerPettyDesktop <spencerpetty@utexas.edu>
2023-04-30 23:54:31 -0500

    minor changes to dqn.py

--- dqn.py
+++ dqn.py
@@ -4,7 +4,7 @@ from expbuffer import ExpBuffer
 
 class DQN(nn.Module):
     
-    def __init__(self, input_size, hidden_nodes, buffer_size = 100):
+    def __init__(self, input_size, hidden_nodes, buffer_size = 100, embedding_dims = 256):
         super().__init__()
         self.layers = nn.Sequential(
             torch.nn.Linear(input_size, hidden_nodes),
@@ -16,6 +16,11 @@ class DQN(nn.Module):
             torch.nn.Linear(hidden_nodes // 4, 1)   # Outputs one real number at end
         )
         self.buffer = ExpBuffer(buffer_size)
+        self.pokeembed = torch.nn.Embedding(num_embeddings = 72, embedding_dim = embedding_dims)
+        self.moveembed = torch.nn.Embedding(num_embeddings = 180, embedding_dim = embedding_dims)
+    
+    def battle_to_embedding(self, battle):
+        pass
     
     def forward(self, x):
         result =  self.layers(x)
